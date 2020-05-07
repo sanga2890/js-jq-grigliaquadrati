ee23b0ac215c839848b5c82c926f28c57d6efa1c
Stefano <stefano2890@gmail.com>
2020-05-07 18:25:35 +0200

    bonus 2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Creare in HTML una griglia di 5x5 quadrati vuoti.
 // Con jQuery, scrivere un numero random tra 0 e 10 dentro ognuno.
 // BONUS 1: generare la griglia con jQuery
-// BONUS 2: se il numero da inserire è pari scriverlo in rosso, se è dispari in nero, se è 0 in verde
+// BONUS 2:
 
 // creo una funzione per generare numeri random;
 function random(min,max) {
@@ -16,6 +16,15 @@ for(var i = 0; i < 25; i++) {
 // sostituisco precendete ciclo do while con each e creo una variabile per i numeri random;
 $('.square').each(function(){
     var random_numbers = random (0 , 10);
-    // ad ogni quadrato aggiungo 1 numero generato;
+
+//BONUS 2: se il numero da inserire è pari scriverlo in rosso, se è dispari in nero, se è 0 in verde
+    if (random_numbers % 2 == 0 && random_numbers != 0){
+        $(this).addClass('red')
+    } else if (random_numbers == 0){
+        $(this).addClass('green')
+    }
+
+
+// ad ogni quadrato aggiungo 1 numero generato;
     $(this).append(random_numbers)
 })
